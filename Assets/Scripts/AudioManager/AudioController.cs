533027d7dcf888c16314dbd660b0f342021c1c9d
kylekant <onhanam1234@gmail.com>
2023-10-12 15:35:38 +0700

    Fix bug play the audio when clicking into the game characters while the game is pausing.

--- Assets/Scripts/AudioManager/AudioController.cs
+++ Assets/Scripts/AudioManager/AudioController.cs
@@ -3,20 +3,25 @@ using UnityEngine;
 public class AudioController : MonoBehaviour
 {
     private AudioSource m_audioSource;
+    private GameManager m_gameManager;
     private void Start()
     {
         m_audioSource = FindObjectOfType<ButtonSound>().GetComponent<AudioSource>();
+        m_gameManager = FindObjectOfType<GameManager>();
     }
     private void OnMouseDown()
     {
-        if (m_audioSource != null)
+        if (m_gameManager.currentGameState == GameState.Play)
         {
-            m_audioSource.PlayOneShot(m_audioSource.clip);
-            Destroy(this.gameObject, m_audioSource.clip.length * 0.5f);
-        }
-        else
-        {
-            Debug.Log("m_AudioSource is null");
+            if (m_audioSource != null)
+            {
+                m_audioSource.PlayOneShot(m_audioSource.clip);
+                Destroy(this.gameObject, m_audioSource.clip.length * 0.5f);
+            }
+            else
+            {
+                Debug.Log("m_AudioSource is null");
+            }
         }
     }
 }
